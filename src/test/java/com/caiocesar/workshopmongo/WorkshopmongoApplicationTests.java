69f99d019cbd9c88799b4417dc33aaa31a0ac5fd
Caio César <caiogalaxy00@gmail.com>
2025-02-11 10:08:53 -0300

    Primeiro commit - projeto criado

---
+++ src/test/java/com/caiocesar/workshopmongo/WorkshopmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.caiocesar.workshopmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
